7a7f35d896a0b1465cf8ab3d8b41144bd68c3a4f
Denny <abalone0204@gmail.com>
2016-01-23 16:16:33 +0800

    driver

--- src/main.js
+++ src/main.js
@@ -1,39 +1,40 @@
 require('../static/stylesheets/style.scss');
 import {
     Observable
-}
-from "rx";
+} from "rx";
 
 
 function main() {
     return {
         DOM: Observable.timer(0, 1000)
             .map(i => `Seconds elapsed ${i}`),
-        // Log: Observable.timer(0, 2000)
-        //     .map(i => 2 * i)
+        Log: Observable.timer(0, 2000)
+            .map(i => 2 * i)
     }
 }
 
-function DOMEffect(text$) {
+function DOMDriver(text$) {
     text$.subscribe(text => {
         const container = document.querySelector('#app');
         container.textContent = text;
     })
 }
 
-function consoleLogEffect(msg$) {
+function consoleLogDriver(msg$) {
     msg$.subscribe(msg => console.log(msg));
 }
 
 function run(mainFn, effects) {
     const sinks = mainFn();
-    DOMEffect(sinks.DOM);
-    consoleLogEffect(sinks.Log);
+    Object.keys(effects)
+    .forEach(key => {
+        effects[key](sinks[key])
+    })
 }
 
-const effectsFunctions = {
-    DOM: DOMEffect,
-    Log: consoleLogEffect,
+const drivers = {
+    DOM: DOMDriver,
+    Log: consoleLogDriver,
 }
 
-run(main);
+run(main, drivers);
