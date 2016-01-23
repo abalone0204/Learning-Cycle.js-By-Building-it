f97f2c563885523cd4b4f879e8bfa61bb21f0653
Denny <abalone0204@gmail.com>
2016-01-23 18:33:47 +0800

    Add subject

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 require('../static/stylesheets/style.scss');
 import {
-    Observable
-} from "rx";
+    Observable, Subject
+}
+from "rx";
 
 
-function main() {
+function main(DOMSource) {
+    const click$ = DOMSource;
     return {
-        DOM: Observable.timer(0, 1000)
-            .map(i => `Seconds elapsed ${i}`),
+        DOM: click$
+        .startWith(null)
+        .flatMapLatest(() =>
+            Observable.timer(0, 1000)
+            .map(i => `Seconds elapsed ${i}`)
+        ), 
         Log: Observable.timer(0, 2000)
             .map(i => 2 * i)
     }
@@ -18,18 +24,23 @@ function DOMDriver(text$) {
         const container = document.querySelector('#app');
         container.textContent = text;
     })
+    const DOMSource = Observable.fromEvent(document, "click");
+    return DOMSource
 }
 
 function consoleLogDriver(msg$) {
     msg$.subscribe(msg => console.log(msg));
 }
 
-function run(mainFn, effects) {
-    const sinks = mainFn();
-    Object.keys(effects)
-    .forEach(key => {
-        effects[key](sinks[key])
-    })
+function run(mainFn, drivers) {
+    const proxyDOMSource = new Subject();
+    const sinks = mainFn(proxyDOMSource);
+    const DOMSource = drivers.DOM(sinks.DOM);
+    DOMSource.subscribe(click => proxyDOMSource.onNext(click))
+        // Object.keys(drivers)
+        // .forEach(key => {
+        //     drivers[key](sinks[key])
+        // })
 }
 
 const drivers = {
