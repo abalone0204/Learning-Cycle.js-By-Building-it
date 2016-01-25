169dcc64b0bfc756b858ec37bb763f6f100f19a2
Denny <abalone0204@gmail.com>
2016-01-26 01:17:38 +0800

    done counter

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 require('../static/stylesheets/style.scss');
 import Cycle from '@cycle/core';
 import {
-    label, input, h1, hr, div, makeDOMDriver
+    button, p, label, div, makeDOMDriver
 }
 from '@cycle/dom';
 
 import Rx from "rx";
 
 function main(sources) {
-
+    const decrementClick$ = sources.DOM
+        .select('#decrement').events('click');
+    const incrementClick$ = sources.DOM
+        .select('#increment').events('click');
+    const decrementAction$ = decrementClick$.map(ev => -1);
+    const incrementAction$ = incrementClick$.map(ev => 1);
+    const number$ = Rx.Observable.of(0)
+        .merge(decrementAction$)
+        .merge(incrementAction$)
+        .scan((prev, cur) => prev+cur);
+    return {
+        DOM: number$.map(number =>
+            div([
+                button('#decrement', 'Decrement'),
+                button('#increment', 'Increment'),
+                p([
+                    label(String(number))
+                ])
+            ])
+        )
+    }
 }
 
 const drivers = {
