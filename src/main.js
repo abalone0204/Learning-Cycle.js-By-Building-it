c43d571df5f464d629a4820d353c31d489c11bba
Denny <abalone0204@gmail.com>
2016-01-25 23:57:28 +0800

    start

--- src/main.js
+++ src/main.js
@@ -8,22 +8,7 @@ from '@cycle/dom';
 import Rx from "rx";
 
 function main(sources) {
-    const inputEv$ = sources.DOM.select('.field').events('input');
-    const name$ = inputEv$
-        .map(ev => ev.target.value)
-        .startWith('World');
-    // return a sinks
-    return {
-        DOM: name$.map(name =>
-            div([
-                label('Name:'),
-                input('.field', {
-                    type: "text"
-                }),
-                hr(),
-                h1(`Hello ${name}!`)
-            ]))
-    }
+
 }
 
 const drivers = {
