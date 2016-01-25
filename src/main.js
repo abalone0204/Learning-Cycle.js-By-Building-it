eaae8c98076ec69aa2c0b0beecb36d242ac71b38
Denny <abalone0204@gmail.com>
2016-01-25 22:32:58 +0800

    migrate to serious vertsion

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 require('../static/stylesheets/style.scss');
 import Cycle from '@cycle/core';
-import {h, h1, span, makeDOMDriver} from './mySimpleCycleDOM.js';
-import {run} from './mySimpleCycle.js';
+// import {h, h1, span, makeDOMDriver} from './mySimpleCycleDOM.js';
+import {h, h1, span, makeDOMDriver} from '@cycle/dom';
+// import {run} from './mySimpleCycle.js';
 
 import {
     Observable, Subject
@@ -10,8 +11,8 @@ from "rx";
 
 // Logics
 function main(sources) {
-    const click$ = sources.DOM.selectEvents('span', 'click');
-    const mouseover$ = sources.DOM.selectEvents('span', 'mouseover');
+    const click$ = sources.DOM.select('span').events('click');
+    const mouseover$ = sources.DOM.select('span').events('mouseover');
     const sinks = {
         DOM: mouseover$
             .startWith(null)
