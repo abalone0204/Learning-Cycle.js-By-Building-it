895bf3ef06f484c9648bbf4f9d8fbe995769cd70
Denny <abalone0204@gmail.com>
2016-01-25 22:33:43 +0800

    start

--- src/main.js
+++ src/main.js
@@ -8,41 +8,11 @@ import {
     Observable, Subject
 }
 from "rx";
-
-// Logics
-function main(sources) {
-    const click$ = sources.DOM.select('span').events('click');
-    const mouseover$ = sources.DOM.select('span').events('mouseover');
-    const sinks = {
-        DOM: mouseover$
-            .startWith(null)
-            .flatMapLatest(() =>
-                Observable.timer(0, 1000)
-                .map(i =>
-                    h1([
-                        span([
-                            `Seconds elapsed ${i}`
-                        ])
-                    ]))
-            ),
-        Log: Observable.timer(0, 2000)
-            .map(i => 2 * i)
-    }
-    return sinks;
-}
-
-// Effects
-
-
-function consoleLogDriver(msg$) {
-    msg$.subscribe(msg => console.log(msg));
-}
-
+ 
 
 
 const drivers = {
     DOM: makeDOMDriver('#app'),
-    Log: consoleLogDriver,
 }
 
 Cycle.run(main, drivers);
