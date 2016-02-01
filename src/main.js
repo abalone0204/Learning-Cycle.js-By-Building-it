60c83a018907acdbc6de681644f5670523bedc76
Denny <abalone0204@gmail.com>
2016-02-02 01:03:54 +0800

    add isolate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require('../static/stylesheets/style.scss');
 import Cycle from '@cycle/core';
+import isolate from '@cycle/isolate';
 import {
     input, label, h1, h4, button, a, div, makeDOMDriver
 }
@@ -11,63 +12,88 @@ from '@cycle/http';
 
 import Rx from "rx";
 
-function intent (DOMSource) {
-    const changeWeight$ = DOMSource.select('.weight').events('input')
-        .map(ev => ev.target.value).startWith(70);
-    const changeHeight$ = DOMSource.select('.height').events('input')
-        .map(ev => ev.target.value).startWith(170);
-    return {changeWeight$,changeHeight$};
+function intent(DOMSource) {
+    const change$ = DOMSource.select('.slider').events('input')
+        .map(ev => ev.target.value);
+    return change$;
 }
 
-function model(changeWeight$, changeHeight$) {
-    const state$ = Rx.Observable.combineLatest(
-        changeWeight$,
-        changeHeight$, (weight, height) => {
-            const heightM = height/100;
-            const bmi = Math.round(weight / (heightM * heightM));
-            return {
-                bmi, weight, height
-            }
-        })
-    return state$;
+function model(upcomingValue$, props$) {
+    const initValue$ = props$.map(props => props.init).first();
+    const value$ = initValue$.concat(upcomingValue$);
+    const state$ = Rx.Observable.combineLatest(value$, props$, (value, props) => {
+        return {
+            label: props.label,
+            unit: props.unit,
+            min: props.min,
+            max: props.max,
+            value: value
+        }
+    })
+    return state$
 }
 
 function view(state$) {
     const vtree$ = state$.map(state =>
-            div([
-                div([
-                    label(`Weight: ${state.weight}kg`),
-                    input('.weight', {
-                        type: 'range',
-                        min: 40,
-                        max: 150,
-                        value: state.weight
-                    })
-                ]),
-                div([
-                    label(`Height: ${state.height}cm`),
-                    input('.height', {
-                        type: 'range',
-                        min: 140,
-                        max: 250,
-                        value: state.height
-                    })
-
-                ]),
-                h1(`BMI is ${state.bmi}`)
-            ])
-        )
+        div('.labled-slider', [
+            label('.label', `${state.label}: ${state.value} ${state.unit}`),
+            input('.slider', {
+                type: 'range',
+                min: state.min,
+                max: state.max,
+                value: state.value
+            })
+        ])
+    )
     return vtree$;
 }
-function main(sources) {
-    const {changeWeight$,changeHeight$} = intent(sources.DOM);
-    const state$ = model(changeWeight$, changeHeight$);
+
+function LabelSlider(sources) {
+    const upcomingValue$ = intent(sources.DOM);
+    const state$ = model(upcomingValue$, sources.props);
     const vtree$ = view(state$);
+    return {
+        DOM: vtree$,
+    }
+}
+
+function main(sources) {
+    const weightProps$ = Rx.Observable.of({
+        label: 'Weight',
+        unit: 'kg',
+        min: 30,
+        max: 220,
+        init: 70
+    })
+    const WeightSlider = isolate(LabelSlider, 'weight');
+    const weightSinks$ = WeightSlider({
+        DOM: sources.DOM,
+        props: weightProps$
+    });
+    const weightVtree$ = weightSinks$.DOM;
+    const heightProps$ = Rx.Observable.of({
+        label: 'Height',
+        unit: 'cm',
+        min: 100,
+        max: 220,
+        init: 170
+    });
+    const HeightSlider = isolate(LabelSlider, 'height');
+    const heightSinks$ = HeightSlider({
+        DOM: sources.DOM,
+        props: heightProps$
+    });
+    const heightVtree$ = heightSinks$.DOM;
+    
+    const vtree$ = Rx.Observable.combineLatest(weightVtree$, heightVtree$, (weightVtree, heightVtree) =>
+        div([
+            weightVtree,
+            heightVtree
+        ]))
     return {
         DOM: vtree$
     }
 }
-
 const drivers = {
     DOM: makeDOMDriver('#app')
 }
